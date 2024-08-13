a31d628f84ff66a7e5264f00cb04c292102d347d
Prem kumar <prem441@gmail.com>
2024-08-13 02:54:25 +0000

    maxpathsum for each offset

--- samples/grid/practice/maxpath.cpp
+++ samples/grid/practice/maxpath.cpp
@@ -21,23 +21,29 @@ int recpathsum(int a[][MAX], int rows, int cols, int max_rows, int max_cols)
 	return total;
 }
 
-int memopathsum(int a[][MAX], unordered_map<string, int> hmap, int rows, int cols, int max_rows, int max_cols)
+int memopathsum(int a[][MAX], unordered_map<string, int> &hmap, int rows, int cols, int max_rows, int max_cols)
 {
+	string key;
         if((rows == max_rows) && (cols == max_cols))
                 return a[rows][cols];
 
         if((rows > max_rows) || (cols > max_cols))
                 return INT_MIN;
 
-	string key = to_string(rows) + '#'+ to_string(cols);
-	if(hmap[key]) {
+	key = to_string(rows) + '#'+ to_string(cols);
+	if(hmap[key] > 1) {
+		cout << key << ":" << "value : " << hmap[key] << "\n";
 		return hmap[key];
 	}
-
+	hmap[key] = 0;
+  
         int side = memopathsum(a, hmap, rows, cols+1, max_rows, max_cols);
         int down = memopathsum(a, hmap, rows+1, cols, max_rows, max_cols);
-
-	hmap[key] = a[rows][cols] + max(side, down);
+	
+	string key2 = to_string(rows) + '#'+ to_string(cols);
+	//cout << "\n" << key2;
+	hmap[key2] = 0;
+	hmap[key2] = a[rows][cols] + max(side, down);
 
         return a[rows][cols] + max(side, down);
 }
@@ -72,7 +78,10 @@ int main() {
 	int dp[MAX][MAX] = {0};
 
 	//cout << "min path sum: " << pathsum(a, 0, 0, rows-1, cols-1);
-	cout << "min path sum: " << memopathsum(a, hmap, 0, 0, rows-1, cols-1);
+	cout << "min path sum: \n" << memopathsum(a, hmap, 0, 0, rows-1, cols-1) << "\n";
+
+	for (auto x : hmap) 
+		cout << x.first << " " << x.second << endl; 
 
 	return 0;
 }
